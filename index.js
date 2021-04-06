704e057c5fec735a9858b9820553ba62596709ee
Bin <binlin35@yahoo.com>
2021-04-05 21:40:08 -0400

    commit tree 9e25d2bf25019b0e777d22d06ae493e70f53acac

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function isWin(win){
+     return win.result === "W";
+}
+
+function superbowlWin(record) {
+    let results = record.find(isWin);
+}
+
+
+superbowlWin.filter(function(year) {return year})
+
+
+
+
+
+
 
